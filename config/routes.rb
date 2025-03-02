335e13e2d8a1446bf02566fccb661f5889dba3fb
Edouard Dieppois <Dieppoisedouard@gmail.com>
2025-03-02 11:52:23 +0100

    Completed

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   root "lists#index"
 
   resources :lists, only: [:new, :create, :show] do
-    resources :bookmarks, only: [:create, :destroy, :new]
+    resources :bookmarks, only: [:create, :new]
   end
+  resources :bookmarks, only: [:destroy]
 end
